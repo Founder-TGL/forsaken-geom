48d8e027c17fa76d1c34d8ffb906203138e6601d
Founder-TGL <ginsbergaiden@gmail.com>
2025-06-29 14:11:18 -0600

    ok got my bug fixed, I forgot to actually make it inhertied, WHILE I WAS FOCUSING on inheritence

--- src/Entity/EntityTypes/CharEntity/CoreCharEntity/CoreCharEntity.h
+++ src/Entity/EntityTypes/CharEntity/CoreCharEntity/CoreCharEntity.h
@@ -1,10 +1,11 @@
 #ifndef CORE_CHAR_ENTITY_H
 #define CORE_CHAR_ENTITY_H
 
+#include "CoreEntity.h"
 #include "Renderable.h"
 #include "EntityRig.h"
 
-class CoreCharEntity
+class CoreCharEntity : CoreEntity
 {
     EntityRig entityRig;
     glm::vec3 position;
